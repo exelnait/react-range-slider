10858e196188847f5554eeb93b1ce3ae1ae36803
Timofey <obidnyak@me.com>
2017-04-23 12:24:19 +0300

    fir commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default App;
